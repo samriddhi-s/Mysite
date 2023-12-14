5aa4306482dc044de1abd513a3ed6c03040a7c5d
samriddhi-s <153799385+samriddhi-s@users.noreply.github.com>
2023-12-14 11:32:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+//import Login from './Component/login';
+import Signup from './Component/signup';
+function App()
+{
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+}
+export default App;
